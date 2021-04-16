35842d51726e68d3760ffaac2693cbfc77e445a8
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-04-16 19:15:41 -0400

    added PORT environment variable and reorganised the code

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const cors = require('cors'); 
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const handle = require('./handlers');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT;
+
+// cors and body-parser are just express middlewares, so when we use them, we initialiase them
+app.use(cors());
+app.use(bodyParser.json()); // we need to parse just the json data.
 
 
 app.get('/', (req, res, next) => {
@@ -10,22 +19,26 @@ app.get('/', (req, res, next) => {
   res.send({ hello: 'world' });
 })
 
-// 404 (Not Found) Error
 // Almost all the express methods like get, use expect an middleware function (the second argument to app.get() below);
-app.use((req, res, next) => {
-  const err = new Error('Not found');
-  err.status = 404;
+// app.use((req, res, next) => {
+//   const err = new Error('Not found');
+//   err.status = 404;
+  
+//   next(err);
+// })
+app.use(handle.notFound);
 
-  next(err);
-})
 
+// 404 (Not Found) Error
 // Middleware (technically an Afterware as it runs after every other request). 
 // Its the final endpoint that the express will hit before server crashes.
-app.use((err, req, res, next) => {
-  res.status(err.status || 500).json({
-    err: err.message || "Something went wrong"
-  })
-})
+// app.use((err, req, res, next) => {
+//   res.status(err.status || 500).json({
+//     err: err.message || "Something went wrong"
+//   })
+// })
+app.use(handle.errors);
+
 
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
